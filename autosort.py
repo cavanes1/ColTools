f693924a1e2714913899fcafcc872c9495a24cdd
cavanes1 <95492449+cavanes1@users.noreply.github.com>
2023-08-14 14:34:40 -0400

    Update autosort.py

--- autosort.py
+++ autosort.py
@@ -23,13 +23,15 @@ if automatic:
     f = open("errener.dat", "r")
     errfile = f.readlines()
     f.close()
+    f = open("energy.all", "r")
+    energies = f.readlines()
+    f.close()
 
 # extract data
 # old name list
 oldnames = []
 for line in oldnamefile:
     currdat = line.split()
-    #oldnames.append([int(currdat[0]), currdat[-1]])
     oldnames.append(currdat[-1])
 # new name list
 newnames = []
@@ -39,18 +41,19 @@ if not automatic:
 # error name list
 errnames = []
 grnderrs = []
+energy = []
 if automatic:
-    #position = 1
     for line in errfile:
         currdat = line.split()
-        #errnames.append([position, line.split()[-1]])
         errnames.append(currdat[-1])
         grnderrs.append(abs(float(currdat[0])))
-        #position += 1
     if errnames == oldnames:
         print("errener.dat has same name order as names.all")
     else:
         print("Inconsistency between errener.dat and names.all")
+    for line in energies:
+        gsener = line.split()
+        energy.append(float(gsener[0]))
 
 # identify relevant files
 mypath = "./"
@@ -59,6 +62,7 @@ grdfiles = []
 for fl in allfiles:
     if "cartgrd." in fl:
         grdfiles.append(fl)
+print("Relevant files:")
 print(grdfiles)
 
 # back up files
@@ -71,22 +75,36 @@ for fl in relfiles:
 
 # automatically create new list
 if automatic:
-    en1 = errnames[:preserve]
-    en2 = errnames[preserve:]
-    ge1 = grnderrs[:preserve]
-    ge2 = grnderrs[preserve:]
+    en1 = errnames[:preserve] # names         to leave unchanged
+    en2 = errnames[preserve:] # names         to sort
+    ge1 = grnderrs[:preserve] # g.s. errors   to leave unchanged
+    ge2 = grnderrs[preserve:] # g.s. errors   to sort
+    eg2 = energy[preserve:]   # g.s. energies to sort
     en3 = [x for _,x in sorted(zip(ge2,en2))]
-    newnames = en1 + en3
+    sortednames = []
+    for i in en3:
+        if i not in sortednames:
+            sortednames.append(i)
+            ci = en2.index(i)
+            cj = ci - 3
+            try:
+                for j in en2[cj:cj + 8]:
+                    if j not in sortednames:
+                        dE = abs(eg2[ci] - eg2[cj])
+                        if dE < 0.00000001:
+                            sortednames.append(j)
+                    cj += 1
+            except:
+                print("Reached end of list while searching for duplicates")
+    newnames = en1 + sortednames
 
 # sort
 for name in newnames:
-    print(name)
     origpos = oldnames.index(name)
     for fl in grdfiles + ["geom.all"]:
         f = open("OLDDATA/" + fl, "r")
         startread = origpos*6
         writethis = f.readlines()[startread:startread + 6]
-        #print(writethis)
         f.close()
         f = open(fl, "a")
         for wt in writethis:
